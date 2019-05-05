e5ddebef81e90b825155f100b26674fbe79a9689
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2019-05-05 14:09:43 -0600

    hamming solution

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,4 +1,12 @@
 class Hamming
   def self.compute(str1, str2)
-  end 
-end 
+    raise ArgumentError if str1.chars.length != str2.chars.length
+    @counter = 0
+    str1.chars.each_index do |i|
+      if str1[i] != str2[i]
+        @counter += 1
+      end
+    end
+    @counter
+  end
+end
