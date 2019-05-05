ab41ffdbb853791a66171007bbccf6133ce0e841
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2019-05-05 14:10:40 -0600

    matrix

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,8 +1,8 @@
 class Matrix
   attr_reader :rows, :columns
 
-  def initialize(matries)
-    @rows = matries.split("\n").map do |row|
+  def initialize(nums)
+    @rows = nums.split("\n").map do |row|
       row.split(" ").map(&:to_i)
     end
     @columns = rows.transpose
